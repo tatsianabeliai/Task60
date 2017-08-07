e31bdcc150882947a50d3c8b7443ecc861bdc49d
TatianaBelyai <tatianabelyai@gmail.com>
2017-08-07 12:05:18 +0300

    fixed the code in accordance with the comments from previous PR as the mistakes were the same

--- src/main/java/TestBase.java
+++ src/main/java/TestBase.java
@@ -1,26 +1,18 @@
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
 import java.util.concurrent.TimeUnit;
 
 public class TestBase {
-    public static final int DEFAULT_TIMEOUT = 10;
-    public WebDriver driver;
+    private static final int DEFAULT_TIMEOUT = 10;
+    WebDriver driver;
 
-    protected String baseUrl;
+    private String baseUrl;
 
-    public TestBase(String baseUrl) {
+    TestBase(String baseUrl) {
         this.baseUrl = baseUrl;
-    }
-
-    public void waitForAllElements() {
-        WebDriverWait waitElements = new WebDriverWait(driver, DEFAULT_TIMEOUT);
-        waitElements.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*")));
     }
 
     @BeforeSuite
